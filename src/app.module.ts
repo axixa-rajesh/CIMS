21b66aea9c6818e85d121cc9c119eb105e6c9e06
jeeshanabbasi <jeeshanabbasi055@gmail.com>
2026-04-27 13:51:53 +0530

    day1

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDatabaseConfig } from './config/database.config';
+import { Module } from '@nestjs/common';
+import {UsersModule} from './users/users.module';  
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        getDatabaseConfig(configService),
+    }),
+  ],
+})
+export class AppModule {}
